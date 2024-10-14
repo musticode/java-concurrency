f28e7cfcda0ab5f58a69280233a609e0e20d518c
mustafakaratas <mustafakaratas4179@gmail.com>
2024-10-14 15:38:59 +0300

    completable future

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws ExecutionException, InterruptedException {
+        //System.out.println( "Hello World!" );
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()-> "test").thenApply(res -> res);
+        System.out.println(completableFuture.get());
     }
 }
