951ace7ba093a6f13160fe952105191d3b22a5bd
mustafakaratas <mustafakaratas4179@gmail.com>
2024-10-25 10:56:34 +0300

    ExecutorService implementation

---
+++ src/main/java/org/example/executorserv/ExecutorServiceDemo.java
@@ -0,0 +1,56 @@
+package org.example.executorserv;
+
+import sun.jvm.hotspot.gc.x.XForwardingEntry;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class ExecutorServiceDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ExecutorService executorService = new ThreadPoolExecutor(1, 5 , 0L,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<Runnable>());
+
+        Callable<String> callableTask = () -> {
+            System.out.println("Call method called");
+            TimeUnit.MILLISECONDS.sleep(2000);
+          return "Task execution is done";
+        };
+
+        Future<String> future = executorService.submit(callableTask);
+        System.out.println(future.get());
+
+
+        List<Callable<String>> callableTasks = new ArrayList<>();
+        callableTasks.add(callableTask);
+        callableTasks.add(callableTask);
+        callableTasks.add(callableTask);
+
+        String result = executorService.invokeAny(callableTasks);
+
+        List<Future<String>> futures = executorService.invokeAll(callableTasks);
+
+        executorService.shutdown();
+
+
+//        List<Runnable> notExecutedTasks = executorService.shutdownNow();
+//
+//        executorService.shutdown();
+//        try {
+//            if (!executorService.awaitTermination(800, TimeUnit.MILLISECONDS)){
+//                executorService.shutdownNow();
+//            }
+//        } catch (InterruptedException e) {
+//            executorService.shutdownNow();
+//        }
+//
+
+
+        Future<String> stringFuture = executorService.submit(callableTask);
+        System.out.println(stringFuture.get());
+        List<Future<String>> stringFutureList = executorService.invokeAll(callableTasks);
+
+
+    }
+}
