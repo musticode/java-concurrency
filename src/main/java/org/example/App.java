bb5894651b01eb8ef999be99b1187c71e661dadd
mustafakaratas <mustafakaratas4179@gmail.com>
2025-04-22 14:09:59 +0300

    print arr test commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,18 +8,16 @@ import java.util.concurrent.ExecutionException;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args ) throws ExecutionException, InterruptedException {
+public class App {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         //System.out.println( "Hello World!" );
-        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(()-> "test").thenApply(res -> res);
+        CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "test").thenApply(res -> res);
         System.out.println(completableFuture.get());
 
         List<List<String>> listOfList = new ArrayList<>();
-        for (List<String> list : listOfList){
-            for (String item : list){
+        for (List<String> list : listOfList) {
+            for (String item : list) {
                 System.out.println(item);
             }
         }
@@ -29,5 +27,19 @@ public class App
                 .flatMap(Collection::stream)
                 .forEach(System.out::println);
 
+        int [] arr = {1,2,3,4,5};
+        printArray(arr);
+
+    }
+
+    static void printArray(int [] arr){
+
+        if (arr.length < 0){
+            throw new IllegalArgumentException("Array is empty");
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
